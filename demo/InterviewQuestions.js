0cc71324afd142eed3bf7bf786a3ebd2a3a720e3
kuldeepsingh000 <kuldeepsingh20203@gmail.com>
2021-06-25 12:40:34 +0530

    Why null

--- demo/InterviewQuestions.js
+++ demo/InterviewQuestions.js
@@ -1,6 +1,11 @@
 // Difference between Null and undefined
-var iAmUseLess = null;
+var iAmUseLess = null; // Null as an assignment value
 console.log(iAmUseLess)
+console.log(typeof(iAmUseLess));
 
 var iAmStandBy;
 console.log(iAmStandBy);
+console.log(typeof(iAmStandBy));
+
+
+// Undefined is a variable that has been declared but not assigned a value
