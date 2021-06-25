538550ae64a2151834e7ea3dc4a02331a37f23dc
kuldeepsingh000 <kuldeepsingh20203@gmail.com>
2021-06-25 13:07:49 +0530

    Done

---
+++ 3ExpressionAndOperators.js
@@ -0,0 +1,18 @@
+// Arithmetic operators
+console.log(5+20); //here 5 is operand and + is operator
+console.log(20/5);
+console.log(20-5);
+console.log(5*20);
+console.log(23%5);
+
+// combination of operator and operand is expressions
+
+var x = 5;
+var y = 5;
+console.log(x == y)
+console.log("is both is equal or not " + x == y);
+console.log(`is both is equal or not : ${x == y} `);
+
+var x = console.log(5*20);
+console.log(x) //Undefined
+
