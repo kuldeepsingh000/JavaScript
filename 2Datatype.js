6a5f9df07bb42d2fd82d5056b88ffe6b1372bbe1
kuldeepsingh000 <kuldeepsingh20203@gmail.com>
2021-06-25 12:12:32 +0530

    started coding in datatype in JS 1

--- 2Datatype.js
+++ 2Datatype.js
@@ -8,5 +8,13 @@ f - Symbol: typeOf instance === "symbol"
 */
 var myName = "Kuldeep Singh";
 console.log(myName);
-
 console.log(typeof(myName))
+
+var iAmKul = true;
+console.log(iAmKul);
+console.log(typeof(iAmKul));
+
+var iAmKul1 = 90;
+console.log(iAmKul);
+console.log(typeof(iAmKul1));
+
