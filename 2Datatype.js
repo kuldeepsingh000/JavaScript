cb7375ffc45e113c4e9350cd8b7a16824516fa80
kuldeepsingh000 <kuldeepsingh20203@gmail.com>
2021-06-25 11:58:52 +0530

    Added dataypes file

---
+++ 2Datatype.js
@@ -0,0 +1,8 @@
+/* Datatypes in javascript
+a - Undefined: typeOf instance === "undefined"
+b - Boolean: typeOf instance === "Boolean"
+c - Number: typeOf instance === "number"
+d - String: typeOf instance === "string"
+e - BigInt: typeOf instance === "bigint"
+f - Symbol: typeOf instance === "symbol"
+*/
