ea6ab01b27e8f9c5bd7e5f6538963ac1bebfc6c3
kuldeepsingh000 <kuldeepsingh20203@gmail.com>
2021-06-25 15:01:40 +0530

    function in javascript

---
+++ 5FunctionsInJavaScript.js
@@ -0,0 +1,9 @@
+// What we will see
+// 1 - Function Definition
+// 2 - Calling a Function
+// 3 - Function Parameter
+// 4 - Function Arguments
+// 5 - Function Expressions
+// 6 - Return Keyword
+// 7 - Anonymous Function
+
