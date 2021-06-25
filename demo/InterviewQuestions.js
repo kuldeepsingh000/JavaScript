8a0ecaedc208d3dbe58c3a4758a9d1028c4ef885
kuldeepsingh000 <kuldeepsingh20203@gmail.com>
2021-06-25 12:50:25 +0530

    Why null

--- demo/InterviewQuestions.js
+++ demo/InterviewQuestions.js
@@ -1,4 +1,4 @@
-// Difference between Null and undefined
+// 1 - Difference between Null and undefined
 var iAmUseLess = null; // Null as an assignment value
 console.log(iAmUseLess)
 console.log(typeof(iAmUseLess));
@@ -7,5 +7,40 @@ var iAmStandBy;
 console.log(iAmStandBy);
 console.log(typeof(iAmStandBy));
 
+/*
+    Output
+    null
+    object
+    undefined
+    undefined
+*/
 
-// Undefined is a variable that has been declared but not assigned a value
+// Undefined is a variable that has been declared but not assigned a value.
+
+//2 -  What is NaN - Not a number
+// it is a property of the global object.
+
+var myPhoneNumber = 9876543210;
+var myName = "Kuldeep Singh";
+
+console.log(myPhoneNumber);
+console.log(myName);
+
+console.log(isNaN(myPhoneNumber));
+console.log(isNaN(myName));
+
+/*
+    Output
+    9876543210
+    Kuldeep Singh
+    false
+    true
+*/
+
+/*
+NaN Practise
+*/
+
+console.log(NaN === NaN);
+console.log(Number.NaN === Number.NaN);
+console.log(isNaN(NaN)); // true
