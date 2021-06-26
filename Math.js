9037508341e27931799d4ba6e0f2e4404ded919d
kuldeepsingh000 <kuldeepsingh20203@gmail.com>
2021-06-26 16:03:26 +0530

    Math.js Done

---
+++ Math.js
@@ -0,0 +1,17 @@
+console.log(Math.PI);
+let num = 10.555;
+console.log(Math.round(num));
+console.log(Math.pow(2, 7));
+console.log(Math.pow(5, 9));
+console.log(Math.sqrt(81));
+console.log(Math.sqrt(66));
+console.log(Math.abs(-55));//Returns the absolute value of a number
+console.log(Math.abs(55));
+console.log(Math.round(7.2));
+console.log(Math.ceil(7.6));//Returns the smallest integer greater than or equal to its numeric argument.
+console.log(Math.floor(7.2));//Returns the greatest integer less than or equal to its numeric argument.
+console.log(Math.min(0, -90, -8990, 536));//Returns the smaller of a set of supplied numeric expressions
+console.log(Math.max(0, 90, -8990, 536));//Returns the larger of a set of supplied numeric expressions.
+console.log(Math.floor(Math.random()*10));
+console.log(Math.trunc(4.6));
+console.log(Math.trunc(-4.6));
