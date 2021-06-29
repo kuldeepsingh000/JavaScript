a7d270d50b48e5354be1f9a7ede4490003df0361
kuldeepsingh000 <kuldeepsingh20203@gmail.com>
2021-06-29 13:53:18 +0530

    OOPS

--- OOPS.js
+++ OOPS.js
@@ -1,3 +1,5 @@
+// Object literal is simply key:value pair
+
 /*
 // First Way to Create Object.
 let bioData = {
@@ -24,5 +26,35 @@ let bioData = {
 console.log(bioData.myName);
 console.log(bioData.myAge);
 bioData.getData();
+
+
+let bio = {
+    myName : {
+        firstName : "Kuldeep",
+        secondName : "Singh",
+    },
+    myAge :26,
+    getData(){
+        console.log(`My name is ${bioData.myName} and my Age is ${bioData.myAge}`);
+    }
+}
+
+console.log(bio.myName);
+console.log(bio.myName.firstName);
+console.log(bio.myName.secondName);
+
+
+// Example of this
+console.log(this); // it refers to the current context and that is window global object
+
+const obj = {
+    myAge : 30,
+    getMyAge(){
+        console.log(this);
+        console.log(this.myAge);
+    }
+}
+
+obj.getMyAge();
 
 
